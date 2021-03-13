30ede5640c96e889a46dea33e4f1e6a46b73e039
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-13 15:14:20 -0600

    change

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
+
 /******************
  * YOUR CODE HERE *
  ******************/
+function addMeToEnd(arr) {
+  return arr.push('Colin')
+}
+
+function addMeToStart(arr) {
+  return arr.unshift('Colin');
+}
+
+function changeLast(arr, value){
+  if (arr.pop()) {
+    return arr += arr.push(value);
+  }
+
+}
+
+function changeAllValuesTo(arr, value) {
+  //const index = arr.indexOf();
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = value;
+  }
+
+}
+
+function oddOrEven(arr) {
+  // const isEven = arr % 2 === 0;
+  // const isOdd = arr % 2 === 1;
+
+  // if (arr === isEven) {
+  //   return 'even';
+  // } 
+  // if (arr === isOdd) {
+  //   return 'odd';
+  // }
+  for (let i = 0; i < arr.length; i ++) {
+    const number = arr[i];
+    if (number % 2 === 0) {
+      return arr[i].push('even');
+    }
+    if (number % 2 === 1) {
+      return arr[i].push('odd');
+    }
+  }
+
+}
 
 
 
